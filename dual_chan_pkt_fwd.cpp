f040a9ffe8a3e2c10c5116dd91efa8dd0d3096bb
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2021-01-11 15:24:26 +0100

    cleanup

--- dual_chan_pkt_fwd.cpp
+++ dual_chan_pkt_fwd.cpp
@@ -1,64 +1,3 @@
- /******************************************************************************
- *
- * Copyright (c) 2015 Thomas Telkamp
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * HBM:
- * Changes for creating a dual channel gateway with the Raspberry Pi+ LoRa(TM) Expansion Board
- * of Uputronics, see also: store.uputronics.com/index.php?route=product/product&product_id=68
- * It now also supports downlink, config file, separate thread for downlink and separate functions
- * in the LoraModem.c file
- *******************************************************************************/
-
-// Raspberry PI pin mapping
-// Pin number in this global_conf.json are Wiring Pi number (wPi colunm)
-// issue a `gpio readall` on PI command line to see mapping
-// +-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+
-// | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
-// +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
-// |     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |
-// |   2 |   8 |   SDA.1 |   IN | 1 |  3 || 4  |   |      | 5V      |     |     |
-// |   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |
-// |   4 |   7 | GPIO. 7 |   IN | 1 |  7 || 8  | 1 | ALT5 | TxD     | 15  | 14  |
-// |     |     |      0v |      |   |  9 || 10 | 1 | ALT5 | RxD     | 16  | 15  |
-// |  17 |   0 | GPIO. 0 |  OUT | 0 | 11 || 12 | 0 | IN   | GPIO. 1 | 1   | 18  |
-// |  27 |   2 | GPIO. 2 |   IN | 0 | 13 || 14 |   |      | 0v      |     |     |
-// |  22 |   3 | GPIO. 3 |   IN | 0 | 15 || 16 | 0 | IN   | GPIO. 4 | 4   | 23  |
-// |     |     |    3.3v |      |   | 17 || 18 | 1 | IN   | GPIO. 5 | 5   | 24  |
-// |  10 |  12 |    MOSI | ALT0 | 0 | 19 || 20 |   |      | 0v      |     |     |
-// |   9 |  13 |    MISO | ALT0 | 0 | 21 || 22 | 0 | IN   | GPIO. 6 | 6   | 25  |
-// |  11 |  14 |    SCLK | ALT0 | 0 | 23 || 24 | 1 | OUT  | CE0     | 10  | 8   |
-// |     |     |      0v |      |   | 25 || 26 | 1 | OUT  | CE1     | 11  | 7   |
-// |   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |
-// |   5 |  21 | GPIO.21 |  OUT | 0 | 29 || 30 |   |      | 0v      |     |     |
-// |   6 |  22 | GPIO.22 |  OUT | 0 | 31 || 32 | 1 | IN   | GPIO.26 | 26  | 12  |
-// |  13 |  23 | GPIO.23 |  OUT | 0 | 33 || 34 |   |      | 0v      |     |     |
-// |  19 |  24 | GPIO.24 |   IN | 0 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |
-// |  26 |  25 | GPIO.25 |   IN | 0 | 37 || 38 | 0 | IN   | GPIO.28 | 28  | 20  |
-// |     |     |      0v |      |   | 39 || 40 | 0 | OUT  | GPIO.29 | 29  | 21  |
-// +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
-// | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
-// +-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+
-// For Uputronics Raspberry Pi+ LoRa(TM) Expansion Board
-// pins configuration in file global_conf.json
-//
-//
-//  "pin_nss": 10,
-//  "pin_dio0": 6,
-//  "pin_nss2": 11,
-//  "pin_dio0_2": 27,
-//  "pin_rst": 0,
-//  "pin_NetworkLED": 22,
-//  "pin_InternetLED": 23,
-//  "pin_ActivityLED_0": 21,
-//  "pin_ActivityLED_1": 29,
-//
-
-
 #include "base64.h"
 #include "parson.h"
 
@@ -89,71 +28,18 @@
 #include <netdb.h>              // gai_strerror 
 #include "LoraModem.h"		// include Lora stuff
 
-// Debug messages
-//int debug = 2;
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
 static const int CHANNEL = 0;
-
 using namespace std;
-
 using namespace rapidjson;
 
 #define BASE64_MAX_LENGTH 341
 
-//static const int SPI_CHANNEL = 0;
-//static const int SPI_CHANNEL_2 = 1;
-
-//bool sx1272 = true;
 typedef unsigned char byte;
-
-//struct sockaddr_in si_other;
-//int s;
-//int slen = sizeof(si_other);
-//struct ifreq ifr;
-
-//uint32_t cp_nb_rx_rcv;
-//uint32_t cp_nb_rx_ok;
-//uint32_t cp_nb_rx_ok_tot;
-//uint32_t cp_nb_rx_bad;
-//uint32_t cp_nb_rx_nocrc;
-//uint32_t cp_up_pkt_fwd;
 uint32_t cp_nb_pull;
 
-//typedef enum SpreadingFactors
-//{
-//    SF7 = 7,
-//    SF8,
-//    SF9,
-//    SF10,
-//    SF11,
-//    SF12
-//} SpreadingFactor_t;
-
-//typedef struct Server
-//{
-//    string address;
-//    uint16_t port;
-//    bool enabled;
-//} Server_t;
-//
-/*******************************************************************************
- *
- * Default values, configure them in global_conf.json
- *
- *******************************************************************************/
-
-// uputronics - Raspberry connections
-// Put them in global_conf.json
-//int ssPin = 0xff;
-//int dio0  = 0xff;
-//int ssPin_2 = 0xff;
-//int dio0_2  = 0xff;
-//int RST   = 0xff;
-//int Led1  = 0xff;
-//int NetworkLED    = 22;
-//int InternetLED   = 23;
-//int ActivityLED_0 = 21;
-//int ActivityLED_1 = 29; 
 
 // Set location in global_conf.json
 float lat =  0.0;
@@ -165,14 +51,6 @@ char platform[24] ;    /* platform definition */
 char email[40] ;       /* used for contact email */
 char description[64] ; /* used for free form description */
 
-// Set spreading factor (SF7 - SF12), &nd  center frequency
-// Overwritten by the ones set in global_conf.json
-//SpreadingFactor_t sf = SF7;
-//uint16_t bw = 125;
-
-// Servers
-//vector<Server_t> servers;
-
 // internet interface
 char interface[6];     // Used to set the interface to communicate to the internet either eth0 or wlan0
 
@@ -180,80 +58,6 @@ static int keepalive_time = DEFAULT_KEEPALIVE; // send a PULL_DATA request every
 /* network protocol variables */
 //static struct timeval push_timeout_half = {0, (PUSH_TIMEOUT_MS * 500)}; /* cut in half, critical for throughput */
 static struct timeval pull_timeout = {0, (PULL_TIMEOUT_MS * 1000)}; /* non critical for throughput */
-
-/* Moved to LoraModem.h check if this can be deleted later
-// #############################################
-// #############################################
-
-#define REG_FIFO                    0x00
-#define REG_FIFO_ADDR_PTR           0x0D
-#define REG_FIFO_TX_BASE_AD         0x0E
-#define REG_FIFO_RX_BASE_AD         0x0F
-#define REG_RX_NB_BYTES             0x13
-#define REG_OPMODE                  0x01
-#define REG_FIFO_RX_CURRENT_ADDR    0x10
-#define REG_IRQ_FLAGS               0x12
-#define REG_DIO_MAPPING_1           0x40
-#define REG_DIO_MAPPING_2           0x41
-#define REG_MODEM_CONFIG            0x1D
-#define REG_MODEM_CONFIG2           0x1E
-#define REG_MODEM_CONFIG3           0x26
-#define REG_SYMB_TIMEOUT_LSB        0x1F
-#define REG_PKT_SNR_VALUE           0x19
-#define REG_PAYLOAD_LENGTH          0x22
-#define REG_IRQ_FLAGS_MASK          0x11
-#define REG_MAX_PAYLOAD_LENGTH      0x23
-#define REG_HOP_PERIOD              0x24
-#define REG_SYNC_WORD               0x39
-#define REG_VERSION                 0x42
-
-#define SX72_MODE_RX_CONTINUOS      0x85
-#define SX72_MODE_TX                0x83
-#define SX72_MODE_SLEEP             0x80
-#define SX72_MODE_STANDBY           0x81
-
-
-#define PAYLOAD_LENGTH              0x40
-
-// LOW NOISE AMPLIFIER
-#define REG_LNA                     0x0C
-#define LNA_MAX_GAIN                0x23
-#define LNA_OFF_GAIN                0x00
-#define LNA_LOW_GAIN                0x20
-
-// CONF REG
-#define REG1                        0x0A
-#define REG2                        0x84
-
-#define SX72_MC2_FSK                0x00
-#define SX72_MC2_SF7                0x70
-#define SX72_MC2_SF8                0x80
-#define SX72_MC2_SF9                0x90
-#define SX72_MC2_SF10               0xA0
-#define SX72_MC2_SF11               0xB0
-#define SX72_MC2_SF12               0xC0
-
-#define SX72_MC1_LOW_DATA_RATE_OPTIMIZE  0x01 // mandated for SF11 and SF12
-
-// FRF
-#define REG_FRF_MSB              0x06
-#define REG_FRF_MID              0x07
-#define REG_FRF_LSB              0x08
-
-#define FRF_MSB                  0xD9 // 868.1 Mhz
-#define FRF_MID                  0x06
-#define FRF_LSB                  0x66
-
-#define BUFLEN 2048  //Max length of buffer
-
-#define PROTOCOL_VERSION  1
-#define PKT_PUSH_DATA 0
-#define PKT_PUSH_ACK  1
-#define PKT_PULL_DATA 2
-
-#define PKT_PULL_RESP 3
-#define PKT_PULL_ACK  4
-*/
 
 #define TX_BUFF_SIZE    2048
 #define STATUS_SIZE     1024
@@ -263,6 +67,7 @@ static struct timeval pull_timeout = {0, (PULL_TIMEOUT_MS * 1000)}; /* non criti
 #define RX_BUFF_SIZE  1024	// Downstream received from MQTT
 uint8_t buff_down[RX_BUFF_SIZE];	// Buffer for downstream
 uint8_t buff_up[RX_BUFF_SIZE];	// Buffer for upstream
+
 /* network sockets */
 //static int sock_up[MAX_SERVERS]; /* sockets for upstream traffic */
 static int sock_down[MAX_SERVERS]; /* sockets for downstream traffic */
@@ -286,15 +91,6 @@ void Die(const char *s)
   perror(s);
   exit(1);
 }
-
-//char * PinName(int pin, char * buff) {
-//  strcpy(buff, "unused");
-//  if (pin != 0xff) {
-//    sprintf(buff, "%d", pin);
-//  }
-//  return buff;
-//}
-
 
 void SetupLoRa(byte CE)
 {
@@ -390,50 +186,6 @@ void SetupLoRa(byte CE)
   WriteRegister(REG_LNA, LNA_MAX_GAIN, CE);  // max lna gain
   WriteRegister(REG_OPMODE, SX72_MODE_RX_CONTINUOS, CE);
 }
-
-//void SolveHostname(const char* p_hostname, uint16_t port, struct sockaddr_in* p_sin)
-//{
- // struct addrinfo hints;
-  //memset(&hints, 0, sizeof(struct addrinfo));
-//  hints.ai_family = AF_INET;
-//  hints.ai_socktype = SOCK_DGRAM;
-//  hints.ai_protocol = IPPROTO_UDP;
-//
-//  char service[6] = { '\0' };
-//  snprintf(service, 6, "%hu", port);
-//
-//  struct addrinfo* p_result = NULL;
-//
-//  // Resolve the domain name into a list of addresses
-//  int error = getaddrinfo(p_hostname, service, &hints, &p_result);
-//  if (error != 0) {
-//      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(error));
-//      exit(EXIT_FAILURE);
-//  }
-//
-//  // Loop over all returned results
-//  for (struct addrinfo* p_rp = p_result; p_rp != NULL; p_rp = p_rp->ai_next) {
-//    struct sockaddr_in* p_saddr = (struct sockaddr_in*)p_rp->ai_addr;
-//    //printf("%s solved to %s\n", p_hostname, inet_ntoa(p_saddr->sin_addr));
-//    p_sin->sin_addr = p_saddr->sin_addr;
-//  }
-//
-//  freeaddrinfo(p_result);
-//}
-
-//void SendUdp(char *msg, int length)
-//{
- // for (vector<Server_t>::iterator it = servers.begin(); it != servers.end(); ++it) {
-  //  if (it->enabled) {
-   //   si_other.sin_port = htons(it->port);
-//
- //     SolveHostname(it->address.c_str(), it->port, &si_other);
-  //    if (sendto(s, (char *)msg, length, 0 , (struct sockaddr *) &si_other, slen)==-1) {
-   //     Die("sendto()");
-    //  }
-    //}
-  //}
-//}
 
 void SendStat()
 {
@@ -546,9 +298,9 @@ int main()
   digitalWrite(RST, HIGH);
   delay(100);
   digitalWrite(RST, LOW);
-  delay(100);   
-initLoraModem(0);
-initLoraModem(1);
+  delay(100);
+  initLoraModem(0);
+  initLoraModem(1);
   //SetupLoRa(0);
   //SetupLoRa(1);
 
@@ -1120,3 +872,5 @@ void thread_down(void* pic) {
 	}
 	printf("\nINFO: End of downstream thread for server  %s.\n",servername);
 }
+
+#pragma clang diagnostic pop
